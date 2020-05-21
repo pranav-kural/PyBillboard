0f74f65da40f6fc82c07b52a3c45ac22d27e1ec7
pranav-kural <pranavkural042@gmail.com>
2020-05-21 07:31:29 -0400

    corrected data loading and testing

--- billboard.py
+++ billboard.py
@@ -1,21 +1,25 @@
 #import beautiful soup
 from bs4 import BeautifulSoup as soup
 # import urlopen
-from urllib.request import urlopen as urlo
+from urllib.request import urlopen, Request
 # import pprint
 import pprint as pp
 
 # URL to billboard current billboard top 100
 page_url = "https://www.billboard.com/charts/hot-100"
 
+# add header to fake as if the request being made by a known browser
+# to bypass mod_security - https://stackoverflow.com/a/16627277/5834653
+page_request = Request(page_url, headers={'User-Agent': 'Mozilla/5.0'})
+
 # open connection and download html
-page_request = urlo(page_url)
+client_object = urlopen(page_request)
 
 # parse html into a soup data structure to be able to traverse html
-page_soup = soup(page_request.read(), "html.parser")
+page_soup = soup(client_object.read(), "html.parser")
 
 # close connection
-page_request.close()
+client_object.close()
 
-## TEST: print out soup contents to verify data
-pp(page_soup)
+## TEST: print out title from soup contents to verify data loaded successfully
+print(page_soup.title)
